f011d3837949eb6184a78b23843c9613a495860e
Taha <taha.hojati@gmail.com>
2018-04-30 12:04:59 -1000

    DetailActivity is done. Just need to enhance the xml a bit.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,19 +1,25 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private static final String TAG = "DetailActivity";
+    private Sandwich mSandwich;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +42,8 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
+        mSandwich = JsonUtils.parseSandwichJson(json);
+        if (mSandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
@@ -45,10 +51,10 @@ public class DetailActivity extends AppCompatActivity {
 
         populateUI();
         Picasso.with(this)
-                .load(sandwich.getImage())
+                .load(mSandwich.getImage())
                 .into(ingredientsIv);
 
-        setTitle(sandwich.getMainName());
+        setTitle(mSandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -57,6 +63,24 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        ((TextView) findViewById(R.id.origin_tv)).setText(mSandwich.getPlaceOfOrigin());
+        ((TextView) findViewById(R.id.description_tv)).setText(mSandwich.getDescription());
+        ((TextView) findViewById(R.id.ingredients_tv)).setText(collectionToString(mSandwich.getIngredients()));
+        ((TextView) findViewById(R.id.also_known_tv)).setText(collectionToString(mSandwich.getIngredients()));
+    }
 
+    private String collectionToString(List<String> list) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return String.join(", ", list);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int index = 0; index < list.size(); ++index) {
+                sb.append(list.get(index));
+                if (index != list.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+            return sb.toString();
+        }
     }
 }
